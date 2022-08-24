409d2dfbb0a896e407084bd1bf279f5a0295efaa
mughal963 <mughal963@gmail.com>
2022-08-24 10:17:33 +0500

    Initial Commit

---
+++ extensions/src/main/java/com/matech/extensions/ExtensionFunction.kt
@@ -0,0 +1,199 @@
+package com.matech.extensions
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
+import android.widget.Toast.makeText
+import androidx.annotation.IdRes
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.DialogFragmentNavigator
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textview.MaterialTextView
+
+fun View.visible(isVisible: Boolean) {
+    visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun View.enable(enabled: Boolean) {
+    isEnabled = enabled
+    alpha = if (enabled) 1f else 0.5f
+}
+
+fun View.clickable(enabled: Boolean) {
+    isClickable = enabled
+    alpha = if (enabled) 1f else 0.75f
+}
+
+
+fun View.setOnSingleClickListener(debounceTime: Long = 4000L, action: () -> Unit) {
+    this.setOnClickListener(object : View.OnClickListener {
+        private var lastClickTime: Long = 0
+
+        override fun onClick(v: View) {
+            val timeNow = SystemClock.elapsedRealtime()
+            val elapsedTimeSinceLastClick = timeNow - lastClickTime
+            if (elapsedTimeSinceLastClick < debounceTime) return
+            else action()
+
+            lastClickTime = SystemClock.elapsedRealtime()
+        }
+    })
+}
+
+fun Fragment.navigateTo(directions: NavDirections) {
+    val controller = findNavController()
+    val currentDestination =
+        (controller.currentDestination as? FragmentNavigator.Destination)?.className
+            ?: (controller.currentDestination as? DialogFragmentNavigator.Destination)?.className
+    if (currentDestination == this.javaClass.name) {
+        controller.navigate(directions)
+    }
+}
+
+fun Fragment.navigate(destination: NavDirections) = with(findNavController()) {
+    currentDestination?.getAction(destination.actionId)
+        ?.let { navigate(destination) }
+}
+
+fun NavController.navigateSafe(
+    @IdRes resId: Int,
+    args: Bundle? = null,
+    navOptions: NavOptions? = null,
+    navExtras: Navigator.Extras? = null
+) {
+    val action = currentDestination?.getAction(resId) ?: graph.getAction(resId)
+    if (action != null && currentDestination?.id != action.destinationId) {
+        navigate(resId, args, navOptions, navExtras)
+    }
+}
+
+fun MaterialTextView.status(value: Int) {
+
+    when (value) {
+        1 -> {
+            setTextColor(Color.parseColor("#2FCF00"))
+            text = giftCardHistoryStatus(value)
+        }
+
+        2 -> {
+            text = giftCardHistoryStatus(value)
+            setTextColor(Color.parseColor("#FF5A01"))
+
+        }
+
+        3 -> {
+            text = giftCardHistoryStatus(value)
+            setTextColor(Color.parseColor("#FF0101"))
+
+        }
+    }
+}
+
+
+fun MaterialTextView.entryFee(fee: Long) {
+    if (fee == 0.toLong()) {
+        text = "free"
+    } else {
+        convertCoinsCount(fee)
+    }
+}
+
+fun RecyclerView.verticalDecoration(
+    context: Context,
+    drawable: Int? = R.drawable.recycler_divider
+) {
+
+    val item = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+    ContextCompat.getDrawable(context, drawable!!)?.let {
+        item.setDrawable(
+            it
+        )
+    }
+    addItemDecoration(item)
+}
+
+fun RecyclerView.horizontalDecoration(
+    context: Context,
+    drawable: Int? = R.drawable.recycler_divider
+) {
+
+    val item = DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL)
+    ContextCompat.getDrawable(context, drawable!!)?.let {
+        item.setDrawable(
+            it
+        )
+    }
+    addItemDecoration(item)
+}
+
+fun RecyclerView.horizontalLayout() {
+    layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
+}
+
+fun RecyclerView.verticalLayout() {
+    layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+}
+
+fun RecyclerView.gridLayout(spanCount: Int) {
+    layoutManager = GridLayoutManager(this.context, spanCount)
+}
+
+
+fun MaterialTextView.setGradientTextColor() {
+    val paint = this.paint
+    val width = paint.measureText(this.text.toString())
+    val textShader: Shader = LinearGradient(
+        0f, 0f, 0f, this.textSize, intArrayOf(
+            Color.parseColor("#2FCF00"),
+            Color.parseColor("#FF1F7723")
+        ), null, Shader.TileMode.CLAMP
+    )
+
+    this.paint.shader = textShader
+}
+
+fun toastMessage(context: Context?, message: String?) {
+    if (!message.isNullOrEmpty()) {
+        val toast = makeText(context, message, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+}
+
+
+fun RadioButton.setStartDrawable(drawable: Int) {
+    setCompoundDrawablesWithIntrinsicBounds(
+        AppCompatResources.getDrawable(
+            this.context,
+            drawable
+        ), null, null, null
+    )
+}
---
+++ extensions/src/main/java/com/matech/extensions/Utils.kt
@@ -0,0 +1,335 @@
+package com.matech.extensions
+
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.net.Uri
+import android.os.Build
+import android.provider.OpenableColumns
+import android.text.format.DateFormat
+import android.util.Log
+import android.util.Patterns
+import android.widget.TextView
+import okhttp3.ResponseBody
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.math.roundToInt
+
+fun setGradientTextColor(textView: TextView) {
+    val paint = textView.paint
+    val width = paint.measureText(textView.text.toString())
+    val textShader: Shader = LinearGradient(
+        0f, 0f, 0f, textView.textSize, intArrayOf(
+            Color.parseColor("#FF9D02"),
+            Color.parseColor("#FF5A01")
+        ), null, Shader.TileMode.CLAMP
+    )
+
+    textView.paint.shader = textShader
+}
+
+fun formatDateTime(timeInMilli: Long): String {
+    val outputDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())/*.apply {
+        timeZone = TimeZone.getTimeZone("UTC")
+    }*/
+    return outputDateFormat.format(timeInMilli)
+}
+
+fun formatDate(timeInMilli: Long): String {
+    val outputDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())/*.apply {
+        timeZone = TimeZone.getTimeZone("UTC")
+    }*/
+    return outputDateFormat.format(timeInMilli)
+}
+
+fun formatMonth(timeInMilli: Long): String {
+    val outputDateFormat = SimpleDateFormat("dd MMM", Locale.getDefault())/*.apply {
+        timeZone = TimeZone.getTimeZone("UTC")
+    }*/
+    return outputDateFormat.format(timeInMilli)
+}
+
+fun isDobValid(timeInMilli: Long): Boolean {
+
+    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+    val date = Date(timeInMilli)
+    val calendar = Calendar.getInstance(TimeZone.getDefault())
+    calendar.time = date
+    val inputYear = calendar.get(Calendar.YEAR)
+    return currentYear - inputYear >= 13
+}
+
+fun getDeviceName(): String? {
+    val manufacturer = Build.MANUFACTURER
+    val brand = Build.BRAND
+    val model = Build.MODEL
+    return if (model.startsWith(manufacturer)) {
+        model
+    } else "$manufacturer $brand $model"
+
+}
+
+// A placeholder email validation check
+fun isEmailValid(email: String): Boolean {
+    return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+}
+
+// A placeholder password validation check
+fun isPasswordValid(password: String): String? {
+
+    if (!password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{7,}$".toRegex())) {
+        return "Minimum seven characters, at least one letter small and capital, one number and one special character"
+    }
+
+    return null
+}
+
+// A placeholder username validation check
+fun isUsernameValid(username: String): Boolean {
+    return username.length > 2
+}
+
+
+// Error Message
+fun responseErrorMessage(errorBody: ResponseBody?): String {
+
+    return try {
+        val errorObject =
+            errorBody?.charStream()?.readText()?.let { JSONObject(it) }
+        errorObject?.getString("message") ?: ""
+    } catch (e: Exception) {
+        Log.e("Exception", e.message.toString())
+        ""
+    }
+}
+
+
+/*fun convertRankCoins(coins: Int): String {
+    return when (coins) {
+        in 1000..999999 -> {
+            val temp = coins / 1000
+            val tempResult = temp.toString()
+            val result: String = tempResult + "K"
+            result
+        }
+        in 1000000..99999999 -> {
+            val temp = coins / 1000000
+            val tempResult = temp.toString()
+            return tempResult + "M"
+        }
+        in 1000000000..100000000000 -> {
+            val temp = coins / 10000000
+            val tempResult = temp.toString()
+            return tempResult + "B"
+        }
+        else -> coins.toString()
+    }
+}*/
+
+
+fun stringArrayToIntArray(value: List<String>): List<Int> {
+    val coins = ArrayList<Int>()
+
+    for (element in value) {
+        //coins[i] = Integer.parseInt(result.coin[i])
+        coins.add(element.toInt())
+    }
+    return coins
+}
+
+const val ISO_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+const val MONTH_YEAR_FORMAT = "MMMM yyyy"
+val locale: Locale = Locale.ENGLISH
+fun profileDateFormat(input: String): String {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val inputFormatter =
+            DateTimeFormatter.ofPattern(ISO_DATE_FORMAT, locale)
+        val outputFormatter = DateTimeFormatter.ofPattern(MONTH_YEAR_FORMAT, locale)
+        val date = LocalDate.parse(input, inputFormatter)
+        val formattedDate = outputFormatter.format(date)
+        formattedDate.toString()
+
+    } else {
+        val parser = SimpleDateFormat(ISO_DATE_FORMAT, locale)
+        val formatter = SimpleDateFormat(MONTH_YEAR_FORMAT, locale)
+        val formattedDate = formatter.format(parser.parse(input)!!)
+        formattedDate.toString()
+    }
+}
+
+fun chatDateFormat(input: String): String {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        try {
+            val inputFormatter =
+                Instant.parse(input)
+            val time = chatDateFormat(inputFormatter.toEpochMilli())
+            time
+
+        } catch (e: Exception) {
+            input
+        }
+
+    } else {
+        val parser = SimpleDateFormat(ISO_DATE_FORMAT, locale)
+        val formatter = SimpleDateFormat("HH:mm", locale)
+        val formattedDate = formatter.format(parser.parse(input)!!)
+        formattedDate.toString()
+    }
+}
+
+fun chatDateFormat(input: Long): String {
+
+    val cal = Calendar.getInstance(Locale.getDefault())
+    cal.timeInMillis = input
+
+    return DateFormat.format("hh:mm a", cal).toString()
+
+}
+
+@SuppressLint("Range")
+fun ContentResolver.getFileName(uri: Uri): String {
+    var name = ""
+    val cursor = query(uri, null, null, null, null)
+    cursor?.use {
+        it.moveToFirst()
+        name = cursor.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+    }
+    return name
+}
+
+fun timerTimeStamp(endTime: Long, currentTime: Long) = (endTime.minus(currentTime)) * 1000
+
+
+fun returnNonZero(value: Double?): Int {
+    return if (value?.roundToInt() == 0) 1
+    else (value?.roundToInt() ?: 1)
+}
+
+fun giftCardHistoryStatus(status: Int): String {
+    if (status == 1) {
+        return "Accepted"
+    }
+    if (status == 2) {
+        return "Pending"
+    }
+    return "Declined"
+}
+
+
+fun timestampToLocalDateTime(time: Long): String {
+    val cal = Calendar.getInstance(Locale.getDefault())
+    cal.timeInMillis = time
+
+    return DateFormat.format("hh:mm a dd MMM", cal).toString()
+
+}
+
+fun timestampToDate(time: Long): String {
+    val cal = Calendar.getInstance(Locale.getDefault())
+    cal.timeInMillis = time
+
+    return DateFormat.format("dd MMM", cal).toString()
+
+}
+
+fun timestampToTime(time: Long): String {
+    val cal = Calendar.getInstance(Locale.getDefault())
+    cal.timeInMillis = time
+
+    return DateFormat.format("hh:mm a", cal).toString()
+
+}
+
+
+fun checkRooted(): Boolean {
+    return checkRootMethod1() || checkRootMethod2() || checkRootMethod3()
+}
+
+private fun checkRootMethod1(): Boolean {
+    val buildTags = Build.TAGS
+    return buildTags != null && buildTags.contains("test-keys")
+}
+
+private fun checkRootMethod2(): Boolean {
+    val paths = arrayOf(
+        "/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su",
+        "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
+        "/system/bin/failsafe/su", "/data/local/su", "/su/bin/su"
+    )
+    for (path in paths) {
+        if (File(path).exists()) return true
+    }
+    return false
+}
+
+private fun checkRootMethod3(): Boolean {
+    var process: Process? = null
+    return try {
+        process = Runtime.getRuntime().exec(arrayOf("/system/xbin/which", "su"))
+        val `in` = BufferedReader(InputStreamReader(process.inputStream))
+        `in`.readLine() != null
+    } catch (t: Throwable) {
+        false
+    } finally {
+        process?.destroy()
+    }
+}
+
+
+fun prettyCount(coins: Long): String {
+    return when (coins) {
+        in 1000..999999 -> {
+            val temp = coins / 1000
+            val tempResult = temp.toString()
+            val result: String = tempResult + "K"
+            result
+        }
+        in 1000000..99999999 -> {
+            val temp = coins / 1000000
+            val tempResult = temp.toString()
+            return tempResult + "M"
+        }
+        in 1000000000..100000000000 -> {
+            val temp = coins / 1000000000
+            val tempResult = temp.toString()
+            return tempResult + "B"
+        }
+        else -> coins.toString()
+    }
+}
+
+fun convertCoinsCount(coins: Long): String {
+    return when (coins) {
+        in 1000..999999 -> {
+            val temp = coins / 1000.0
+            val tempResult = if (temp % 1.0 == 0.0) {
+                String.format("%.0f", temp)
+            } else {
+                String.format("%.1f", temp)
+            }
+
+            val result: String = tempResult + "K"
+            result
+        }
+        in 1000000..99999999 -> {
+            val temp = coins / 1000000.0
+            val tempResult = String.format("%.1f", temp)
+            return tempResult + "M"
+        }
+        in 1000000000..100000000000 -> {
+            val temp = coins / 1000000000
+            return "$temp B"
+
+        }
+        else -> coins.toString()
+    }
+}
